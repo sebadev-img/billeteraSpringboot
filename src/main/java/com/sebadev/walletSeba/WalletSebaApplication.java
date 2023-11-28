51a70bf569e84fe80a3d109e306070e53840e9ac
sebastian <sebastiandev.img@gmail.com>
2023-11-27 23:55:06 -0300

    proyecto base con config de database

---
+++ src/main/java/com/sebadev/walletSeba/WalletSebaApplication.java
@@ -0,0 +1,13 @@
+package com.sebadev.walletSeba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletSebaApplication.class, args);
+	}
+
+}
